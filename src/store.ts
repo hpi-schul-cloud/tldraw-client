0b2505ecf1647e1e7d63012fdcf02867dafb1bb5
Błażej Szczepanowski <blazej1679@gmail.com>
2023-04-20 14:57:50 +0200

    update store

--- src/store.ts
+++ src/store.ts
@@ -1,29 +1,12 @@
-import * as Y from "yjs";
-import { WebsocketProvider } from "y-websocket";
-import { TDBinding, TDShape } from "@tldraw/tldraw";
+import * as Y from 'yjs'
+import { WebsocketProvider } from 'y-websocket'
+import { TDBinding, TDShape } from '@tldraw/tldraw'
 
-const VERSION = 1;
-
-// Create the doc
-export const doc = new Y.Doc();
-
-export const roomID = `y-tldraw-${VERSION}`;
-
-// Create a websocket provider
-export const provider = new WebsocketProvider(
-  "wss://demos.yjs.dev",
-  roomID,
-  doc,
-  {
-    connect: true
-  }
-);
-
-// Export the provider's awareness API
-export const awareness = provider.awareness;
-
-export const yShapes: Y.Map<TDShape> = doc.getMap("shapes");
-export const yBindings: Y.Map<TDBinding> = doc.getMap("bindings");
-
-// Create an undo manager for the shapes and binding maps
-export const undoManager = new Y.UndoManager([yShapes, yBindings]);
+export const doc = new Y.Doc()
+export const urlParams = new URLSearchParams(window.location.search);
+export const roomID = urlParams.get('room_name') ?? 'GLOBAL';
+export const provider = new WebsocketProvider(`ws://localhost:3333`, roomID, doc)
+export const awareness = provider.awareness
+export const yShapes: Y.Map<TDShape> = doc.getMap('shapes')
+export const yBindings: Y.Map<TDBinding> = doc.getMap('bindings')
+export const undoManager = new Y.UndoManager([yShapes, yBindings])
