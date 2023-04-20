ee8efa86822964efe8652e56be5d55ce43052d28
Mateusz Sapala <sapala.mateusz@gmail.com>
2023-04-20 17:13:55 +0200

    Add prefix

--- src/store.ts
+++ src/store.ts
@@ -5,7 +5,7 @@ import { TDBinding, TDShape } from '@tldraw/tldraw'
 export const doc = new Y.Doc()
 export const urlParams = new URLSearchParams(window.location.search);
 export const roomID = urlParams.get('room_name') ?? 'GLOBAL';
-export const provider = new WebsocketProvider(`ws://localhost:3333`, roomID, doc)
+export const provider = new WebsocketProvider(`ws://localhost:3333/tldraw-server`, roomID, doc)
 export const awareness = provider.awareness
 export const yShapes: Y.Map<TDShape> = doc.getMap('shapes')
 export const yBindings: Y.Map<TDBinding> = doc.getMap('bindings')
