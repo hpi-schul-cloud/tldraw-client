a38a7bdf4a0ef62e85e17ec5ff5af70491a22ddb
Viktoriia <1>
2023-07-10 10:22:12 +0200

    Add workflows/test

--- src/store/store.ts
+++ src/store/store.ts
@@ -6,7 +6,6 @@ export const doc = new Y.Doc()
 export const urlParams = new URLSearchParams(window.location.search);
 export const roomID = urlParams.get('roomName') ?? 'GLOBAL';
 export const provider = new WebsocketProvider(`ws://localhost:3345`, roomID, doc, {
-    connect: true
   })
 export const awareness = provider.awareness;
 export const yShapes: Y.Map<TDShape> = doc.getMap('shapes')
