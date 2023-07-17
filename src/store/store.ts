c4539da4204087947bb57e1712a7dc1599048702
Viktoriia <1>
2023-07-17 11:32:47 +0200

    Fix comment

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,12 @@
-import * as Y from 'yjs';
-import { TDBinding, TDShape } from '@tldraw/tldraw';
+import { Doc, Map, UndoManager } from 'yjs';
 import { WebsocketProvider } from 'y-websocket';
+import { TDBinding, TDShape } from '@tldraw/tldraw';
 
-export const doc = new Y.Doc();
+export const doc = new Doc();
 export const urlParams = new URLSearchParams(window.location.search);
 export const roomID = urlParams.get('roomName') ?? 'GLOBAL';
 export const provider = new WebsocketProvider(`ws://localhost:3345`, roomID, doc, {});
 export const awareness = provider.awareness;
-export const yShapes: Y.Map<TDShape> = doc.getMap('shapes');
-export const yBindings: Y.Map<TDBinding> = doc.getMap('bindings');
-export const undoManager = new Y.UndoManager([yShapes, yBindings]);
+export const yShapes: Map<TDShape> = doc.getMap('shapes');
+export const yBindings: Map<TDBinding> = doc.getMap('bindings');
+export const undoManager = new UndoManager([yShapes, yBindings]);
