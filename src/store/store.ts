24f2e8b01d0e9d764d5d2b92798db8e78bc88941
Viktoriia <1>
2023-07-17 11:58:50 +0200

    fix comments

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,22 @@ import { Doc, Map, UndoManager } from 'yjs';
 import { WebsocketProvider } from 'y-websocket';
 import { TDBinding, TDShape } from '@tldraw/tldraw';
 
+const defaultOptions = {
+	roomName: 'GLOBAL',
+	websocketUrl: 'ws://localhost:3345',
+};
+
 export const doc = new Doc();
 export const urlParams = new URLSearchParams(window.location.search);
-export const roomID = urlParams.get('roomName') ?? 'GLOBAL';
-export const provider = new WebsocketProvider(`ws://localhost:3345`, roomID, doc, {});
+export let roomID = urlParams.get('roomName') ?? defaultOptions.roomName;
+export let provider = new WebsocketProvider(defaultOptions.websocketUrl, roomID, doc, {});
 export const awareness = provider.awareness;
 export const yShapes: Map<TDShape> = doc.getMap('shapes');
 export const yBindings: Map<TDBinding> = doc.getMap('bindings');
 export const undoManager = new UndoManager([yShapes, yBindings]);
+
+export function configure(options: any) {
+	Object.assign(defaultOptions, options);
+	roomID = urlParams.get('roomName') ?? defaultOptions.roomName;
+	provider = new WebsocketProvider(defaultOptions.websocketUrl, roomID, doc, {});
+}
