a3c9a6d87eccdbc18f0de2a67a2a7437a3e14b87
Viktoriia <1>
2023-08-10 10:41:16 +0200

    Fetch tldraw Server url

--- src/store/store.ts
+++ src/store/store.ts
@@ -6,6 +6,24 @@ const defaultOptions = {
 	roomName: 'GLOBAL',
 	websocketUrl: 'ws://localhost:3345',
 };
+async function fetchTldrawServerURL() {
+	try {
+		const response = await fetch(
+			`${window.location.origin}/tldraw-client-runtime.config.json`,
+		);
+		const data = await response.json();
+		return data.tldrawServerURL;
+	} catch (error) {
+		console.error('Error fetching tldrawServerURL:', error);
+		return null;
+	}
+}
+
+fetchTldrawServerURL().then((tldrawServerURL) => {
+	if (tldrawServerURL) {
+		defaultOptions.websocketUrl = tldrawServerURL;
+	}
+});
 
 export const doc = new Doc();
 export const urlParams = new URLSearchParams(window.location.search);
@@ -16,6 +34,7 @@ export let provider = new WebsocketProvider(
 	doc,
 	{},
 );
+
 export const awareness = provider.awareness;
 export const yShapes: Map<TDShape> = doc.getMap('shapes');
 export const yBindings: Map<TDBinding> = doc.getMap('bindings');
