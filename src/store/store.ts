16ef94220a8b235197bbc66e352ec827e52b358b
Viktoriia <1>
2023-08-14 10:17:06 +0200

    Delete fetch

--- src/store/store.ts
+++ src/store/store.ts
@@ -6,24 +6,6 @@ const defaultOptions = {
 	roomName: 'GLOBAL',
 	websocketUrl: 'ws://localhost:3345',
 };
-async function fetchTldrawServerURL() {
-	try {
-		const response = await fetch(
-			`${window.location.origin}/tldraw-client-runtime.config.json`,
-		);
-		const data = await response.json();
-		return data.tldrawServerURL;
-	} catch (error) {
-		console.error('Error fetching tldrawServerURL:', error);
-		return null;
-	}
-}
-
-fetchTldrawServerURL().then((tldrawServerURL) => {
-	if (tldrawServerURL) {
-		defaultOptions.websocketUrl = tldrawServerURL;
-	}
-});
 
 export const doc = new Doc();
 export const urlParams = new URLSearchParams(window.location.search);
