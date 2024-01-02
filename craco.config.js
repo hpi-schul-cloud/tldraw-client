d7ab6d9ffc5179f9028f82b09b133f5d658eef3f
davwas <daw.wasik@gmail.com>
2024-01-02 14:09:15 +0100

    add csp

---
+++ craco.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+class NoncePlaceholderPlugin {
+  apply(compiler) {
+    compiler.hooks.thisCompilation.tap(
+      'NoncePlaceholderPlugin',
+      (compilation) => {
+        HtmlWebpackPlugin.getHooks(compilation).afterTemplateExecution.tapAsync(
+          'NoncePlaceholderPlugin',
+          (data, cb) => {
+            const { headTags } = data;
+            headTags.forEach((x) => {
+              x.attributes.nonce = '**CSP_NONCE**';
+            });
+            cb(null, data);
+          }
+        );
+      }
+    );
+  }
+}
+
+module.exports = {
+  webpack: {
+    plugins: {
+      add: [
+        new NoncePlaceholderPlugin(),
+      ],
+    },
+  },
+};
