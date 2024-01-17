be53ee8cc003d9bdb576452385a0188b5ea7f639
davwas <daw.wasik@gmail.com>
2024-01-17 21:58:24 +0100

    fix users count hook

--- src/hooks/useUsersCount.ts
+++ src/hooks/useUsersCount.ts
@@ -1,16 +1,20 @@
 import { useEffect, useState } from "react";
+import { User } from "@y-presence/client";
+import { UserPresence } from "../types/UserPresence";
 import { room } from "../stores/setup";
 
 export function useUsersCount() {
   const [usersCount, setUsersCount] = useState(0);
 
+  const handleUsersChange = (users: User<UserPresence>[]) => {
+    setUsersCount(users.length);
+  };
+
   useEffect(() => {
-    const unsubUsers = room.subscribe("users", (users) => {
-      setUsersCount(users.length);
-    });
+    room.subscribe("users", handleUsersChange);
 
     return () => {
-      unsubUsers();
+      room.unsubscribe("users", handleUsersChange);
     };
   }, []);
 
