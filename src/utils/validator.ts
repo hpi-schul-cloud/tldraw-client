19037b17ea6606e35640bf31e1a2c2417e0eb2ea
Tomasz Wiaderek <Tomasz.Wiaderek@gca.pass-consulting.com>
2024-02-08 16:35:41 +0100

    fix regex

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -3,7 +3,7 @@ import { setErrorData } from "./errorData";
 import { redirectToErrorPage } from "./redirectUtils";
 
 export const validateId = (id: string) => {
-  if (!id.match("/^[a-f0-9]{24}$/i")) {
+  if (!id.match("/^[a-f0-9]{24}$")) {
     setErrorData(HttpStatusCode.NotFound, "tldraw.error.ws.404");
     redirectToErrorPage();
   }
