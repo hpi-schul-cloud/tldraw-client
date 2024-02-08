b0a28278bd27935f660c0412c776681123801091
Tomasz Wiaderek <Tomasz.Wiaderek@gca.pass-consulting.com>
2024-02-08 16:58:35 +0100

    fix regex

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -3,7 +3,7 @@ import { setErrorData } from "./errorData";
 import { redirectToErrorPage } from "./redirectUtils";
 
 export const validateId = (id: string) => {
-  if (!id.match("/^[a-f0-9]{24}$")) {
+  if (!/^[a-f\d]{24}$/i.test(id)) {
     setErrorData(HttpStatusCode.NotFound, "tldraw.error.ws.4404");
     redirectToErrorPage();
   }
