cfb276e4d74c07ceafaf84553841376f073aae34
blazejpass <blazej.szczepanowski@gca.pass-consulting.com>
2024-02-22 13:15:13 +0100

    Change parent type for file uploads

--- src/configuration/api/api.configuration.ts
+++ src/configuration/api/api.configuration.ts
@@ -1,5 +1,5 @@
 export const API = {
-  FILE_UPLOAD: "/api/v3/file/upload/SCHOOLID/boardnodes/CONTEXTID",
+  FILE_UPLOAD: "/api/v3/file/upload/SCHOOLID/drawingnodes/CONTEXTID",
   LOGIN_REDIRECT: "/login?redirect=/tldraw?roomName=ROOMID",
   USER_DATA: `/api/v3/user/me`,
   ENV_CONFIG: `/api/v1/config/app/public`,
