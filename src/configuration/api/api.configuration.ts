9237896aef7b73efa7a08a35b77a45371e52e4e1
Max Bischof <106820326+bischofmax@users.noreply.github.com>
2024-03-26 10:29:01 +0100

    BC-6676 - Use v3 config (#67)

--- src/configuration/api/api.configuration.ts
+++ src/configuration/api/api.configuration.ts
@@ -2,5 +2,5 @@ export const API = {
   FILE_UPLOAD: "/api/v3/file/upload/SCHOOLID/boardnodes/CONTEXTID",
   LOGIN_REDIRECT: "/login?redirect=/tldraw?roomName=ROOMID",
   USER_DATA: `/api/v3/user/me`,
-  ENV_CONFIG: `/api/v1/config/app/public`,
+  ENV_CONFIG: `/api/v3/config/public`,
 };
--- src/utils/envConfig.ts
+++ src/utils/envConfig.ts
@@ -1,16 +1,24 @@
-import { Envs } from "../types/Envs";
-import { API } from "../configuration/api/api.configuration";
+import { Envs } from "../types";
+import { API } from "../configuration";
+import { ConfigurationMapper } from "../mapper";
+import { HttpGuard } from "../guards";
 
-export const getEnvs = async (): Promise<Envs | undefined> => {
+// the try catch should not part of getEnvs, the place that use it must handle the errors
+// should be part of a store
+// Without loading the config the Promise.all should not be finished and proceed.
+export const getEnvs = async (): Promise<Envs|undefined> => {
   try {
+    // TODO: check order..
     const response = await fetch(API.ENV_CONFIG);
+    HttpGuard.checkStatusOk(response);
+    const responseData = await response.json();
+   
+    const configuration = ConfigurationMapper.mapToConfigurationFromResponse(responseData);
 
-    if (!response.ok) {
-      throw new Error(`${response.status} - ${response.statusText}`);
-    }
-
-    return await response.json();
+    return configuration;
   } catch (error) {
+    // It should exists one place that execute the console.error in the application. A errorHandler.
+    // If we want to collect this informations to send it back to us, then we have currently no possibility to implement it.
     console.error("Error fetching env config:", error);
   }
 };
