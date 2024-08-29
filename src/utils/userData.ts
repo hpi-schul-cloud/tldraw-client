12df4e0ba3c004656626aac04b72228e3af1c539
dyedwiper <maximilian.becker@dataport.de>
2024-08-29 14:35:40 +0200

    Update error handling in getUserData

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,30 +1,29 @@
-import { User, UserResult } from "../types/User";
-import { HttpStatusCode } from "../types/StatusCodeEnums";
 import { API } from "../configuration/api/api.configuration";
+import { HttpStatusCode } from "../types/StatusCodeEnums";
+import { User, UserResult } from "../types/User";
+import { setErrorData } from "./errorData";
+import { redirectToErrorPage, redirectToLoginPage } from "./redirectUtils";
 
 export const getUserData = async (): Promise<UserResult> => {
-  const userResult: UserResult = {
-    user: undefined,
-    statusCode: HttpStatusCode.InternalServerError,
-  };
+  const response = await fetch(API.USER_DATA);
 
-  try {
-    const response = await fetch(API.USER_DATA);
-    userResult.statusCode = response.status;
-
-    if (!response.ok) {
-      throw new Error(`${response.status} - ${response.statusText}`);
-    }
-
-    const data: User = await response.json();
-    data.initials =
-      data.firstName.charAt(0).toUpperCase() +
-      data.lastName.charAt(0).toUpperCase();
-
-    userResult.user = data;
-  } catch (error) {
-    console.error("Error fetching user data:", error);
+  if (response.status === HttpStatusCode.Unauthorized) {
+    redirectToLoginPage();
+    throw new Error("Unauthorized");
   }
+
+  if (!response.ok) {
+    setErrorData(HttpStatusCode.InternalServerError, "error.500");
+    redirectToErrorPage();
+    throw new Error("Internal Server Error");
+  }
+
+  const data: User = await response.json();
+  data.initials =
+    data.firstName.charAt(0).toUpperCase() +
+    data.lastName.charAt(0).toUpperCase();
+
+  const userResult = { user: data, statusCode: response.status };
 
   return userResult;
 };
