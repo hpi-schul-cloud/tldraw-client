c22c74809c1f3a52d13b265c9b03cde4d586070d
SevenWaysDP <sergej.hoffmann@dataport.de>
2024-10-18 09:26:53 +0200

    Refactor configuration API to use SERVER_TLDRAW_2_ENABLED
    - Update tldraw-client-configmap.yml.j2 to use SERVER_TLDRAW_2_ENABLED instead of VITE_SERVER_TLDRAW_2_ENABLED
    - Add location block in nginx.conf.template to return SERVER_TLDRAW_2_ENABLED value
    - Modify api.configuration.ts to fetch SERVER_TLDRAW_2_ENABLED value from /tldraw-client-runtime.config.json in production environment
    - Remove unused code related to BC-7906

--- src/configuration/api/api.configuration.ts
+++ src/configuration/api/api.configuration.ts
@@ -1,15 +1,50 @@
-const configApiUrl = () => {
-  const configApiUrl = import.meta.env.VITE_SERVER_TLDRAW_2_ENABLED
+// remove this code by BC-7906
+// set ENV_CONFIG to /api/tldraw/config/public
+// remove line 7 and 8 in ngnix.conf.template
+import { HttpStatusCode } from "../../types/StatusCodeEnums";
+import { setErrorData } from "../../utils/errorData";
+import { redirectToErrorPage } from "../../utils/redirectUtils";
+
+const getConfigOptions = async (): Promise<{
+  SERVER_TLDRAW_2_ENABLED: boolean;
+}> => {
+  const connectionOptions = {
+    SERVER_TLDRAW_2_ENABLED: !!import.meta.env.VITE_SERVER_TLDRAW_2_ENABLED,
+  };
+
+  if (import.meta.env.PROD) {
+    try {
+      const response = await fetch("/tldraw-client-runtime.config.json");
+
+      if (!response.ok) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      }
+
+      const data: { SERVER_TLDRAW_2_ENABLED: string } = await response.json();
+      connectionOptions.SERVER_TLDRAW_2_ENABLED =
+        data.SERVER_TLDRAW_2_ENABLED === "true";
+    } catch (error) {
+      setErrorData(HttpStatusCode.InternalServerError, "error.500");
+      redirectToErrorPage();
+    }
+  }
+
+  return connectionOptions;
+};
+
+const configApiUrl = async () => {
+  const configApiUrl = (await getConfigOptions()).SERVER_TLDRAW_2_ENABLED
     ? `/api/tldraw/config/public`
     : `/api/v3/config/public`;
 
   return configApiUrl;
 };
+
 export const API = {
   FILE_UPLOAD: "/api/v3/file/upload/school/SCHOOLID/boardnodes/CONTEXTID",
   FILE_DELETE: "/api/v3/file/delete/FILERECORD_ID",
   FILE_RESTORE: "/api/v3/file/restore/FILERECORD_ID",
   LOGIN_REDIRECT: "/login?redirect=/tldraw?parentId=PARENTID",
   USER_DATA: `/api/v3/user/me`,
-  ENV_CONFIG: configApiUrl(),
+  ENV_CONFIG: await configApiUrl(),
 };
