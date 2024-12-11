925b25b65cf740ec33fe93250d850a35afeed3ac
Max Bischof <maximilian.bischof@dataport.de>
2024-12-11 11:50:30 +0100

    Remove unecessary config handling

--- src/configuration/api/api.configuration.ts
+++ src/configuration/api/api.configuration.ts
@@ -1,41 +1,8 @@
-// remove this code by BC-7906
-// set CONFIG_PATH to /api/tldraw/config/public
-// remove line 7 and 8 in ngnix.conf.template
-import { HttpStatusCode } from "../../types/StatusCodeEnums";
-import { setErrorData } from "../../utils/errorData";
-import { redirectToErrorPage } from "../../utils/redirectUtils";
-
-const getConfigOptions = async (): Promise<{
-  CONFIG_PATH: string;
-}> => {
-  const connectionOptions = {
-    CONFIG_PATH: `/api/tldraw/config/public`,
-  };
-
-  if (import.meta.env.PROD) {
-    try {
-      const response = await fetch("/tldraw-client-runtime.config.json");
-
-      if (!response.ok) {
-        throw new Error(`${response.status} - ${response.statusText}`);
-      }
-
-      const data: { CONFIG_PATH: string } = await response.json();
-      connectionOptions.CONFIG_PATH = data.CONFIG_PATH;
-    } catch (error) {
-      setErrorData(HttpStatusCode.InternalServerError, "error.500");
-      redirectToErrorPage();
-    }
-  }
-
-  return connectionOptions;
-};
-
 export const API = {
   FILE_UPLOAD: "/api/v3/file/upload/school/SCHOOLID/boardnodes/CONTEXTID",
   FILE_DELETE: "/api/v3/file/delete/FILERECORD_ID",
   FILE_RESTORE: "/api/v3/file/restore/FILERECORD_ID",
   LOGIN_REDIRECT: "/login?redirect=/tldraw?parentId=PARENTID",
   USER_DATA: `/api/v3/user/me`,
-  CONFIG_PATH: await getConfigOptions().then((options) => options.CONFIG_PATH),
+  CONFIG_PATH: `/api/tldraw/config/public`,
 };
